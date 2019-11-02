1dc7c29d7a4d34d5d078dce49a6b3a721cfca2c9
Ackerley Tng <ackerleytng@gmail.com>
2019-11-02 21:05:20 +0800

    Add yaml link declarations

--- apicentre.py
+++ apicentre.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python
+
 import os
+import sys
+import click
+import yaml
 from jinja2 import Template
 from pydantic import BaseModel
+
+
+class LinkInfo(BaseModel):
+    path: str
+    label: str
+    addr: str
 
 def render(links):
     with open("template.js") as file_:
@@ -8,17 +19,14 @@ def render(links):
 
     return template.render(links=links)
 
-class LinkInfo(BaseModel):
-    path: str
-    label: str
-    addr: str
+@click.command()
+@click.argument('link_info_path', type=click.File('rb'))
+def main(link_info_path):
+    link_info_list = yaml.load(link_info_path.read(), Loader=yaml.SafeLoader)
 
-def main():
+    links = [LinkInfo(**l) for l in link_info_list]
+
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    links = [
-        LinkInfo(path="pyjwt", label="Pyjwt", addr="https://pyjwt.readthedocs.io/en/latest/usage.html"),
-    ]
-
     with open(os.path.join(script_dir, "src", "App.js"), "w") as f:
         f.write(render(links))
 
